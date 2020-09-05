78a588ab8124c5f25cd2e76c9593d9e1dfce3411
shisiying <shisiying@xhzyxed.cn>
2020-09-05 22:54:51 +0800

    feat:公共组件&骨架搭建

---
+++ global/setting.go
@@ -0,0 +1,14 @@
+package global
+
+import (
+	"github.com/shisiying/go-blog/pkg/logger"
+	"github.com/shisiying/go-blog/pkg/setting"
+)
+
+var (
+	ServerSetting *setting.ServerSettingS
+	AppSetting *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+
+	Logger *logger.Logger
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/natefinch/lumberjack"
+	"github.com/shisiying/go-blog/global"
+	"github.com/shisiying/go-blog/internal/model"
+	"github.com/shisiying/go-blog/internal/routers"
+	"github.com/shisiying/go-blog/pkg/logger"
+	"github.com/shisiying/go-blog/pkg/setting"
+	"log"
+	"net/http"
+	"time"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err:%v", err)
+	}
+
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err:%v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDbEngine err: %v", err)
+	}
+}
+
+func setupLogger() error {
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:global.AppSetting.LogSavePath+"/"+global.AppSetting.LogFileName+global.AppSetting.LogFileExt,
+		MaxSize:600,
+		MaxAge:10,
+		LocalTime:true,
+	}, "", log.LstdFlags).WithCaller(2)
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeOut,
+		WriteTimeout:   global.ServerSetting.WriteTimeOut,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	s, err := setting.NewSetting()
+
+	if err != nil {
+		return err
+	}
+
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = s.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeOut *= time.Second
+	global.ServerSetting.WriteTimeOut *= time.Second
+
+	return nil
+
+}
