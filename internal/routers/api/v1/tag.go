f4c1142d6c9c41b7a9f463d7caa23dfafb197943
shisiying <shisiying@xhzyxed.cn>
2020-09-09 23:10:15 +0800

    feat:dao&service接口编写

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -1,6 +1,12 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shisiying/go-blog/global"
+	"github.com/shisiying/go-blog/internal/service"
+	"github.com/shisiying/go-blog/pkg/app"
+	"github.com/shisiying/go-blog/pkg/errcode"
+)
 
 type Tag struct{}
 
@@ -19,7 +25,32 @@ func NewTag() Tag {
 // @Failure 500 {object} errcode.Error "内部错误"
 // @Router /api/v1/tags [get]
 func (t Tag) List(c *gin.Context) {
+	param := service.TagListRequest{}
+	response := app.NewResponse(c)
+	valid, errs := app.BindAndValid(c, &param)
+	if !valid {
+		global.Logger.Errorf(c, "app.BindAndValid errs: %v", errs)
+		response.ToErrorResponse(errcode.InvalidParams.WithDetails(errs.Errors()...))
+		return
+	}
 
+	svc := service.New(c.Request.Context())
+	pager := app.Pager{Page: app.GetPage(c), PageSize: app.GetPageSize(c)}
+	totalRows, err := svc.CountTag(&service.CountTagRequest{Name: param.Name, State: param.State})
+	if err != nil {
+		global.Logger.Errorf(c, "svc.CountTag err: %v", err)
+		response.ToErrorResponse(errcode.ErrorCountTagFail)
+		return
+	}
+	tags, err := svc.GetTagList(&param, &pager)
+	if err != nil {
+		global.Logger.Errorf(c, "svc.GetTagList err: %v", err)
+		response.ToErrorResponse(errcode.ErrorGetTagListFail)
+		return
+	}
+
+	response.ToResponseList(tags, totalRows)
+	return
 }
 
 // @Summary 新增标签
